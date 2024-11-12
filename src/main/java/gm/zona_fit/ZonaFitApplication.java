a5625a3554e90e4e35f15f59de0baaf0ba46a02d
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-11-11 20:41:45 -0600

    first commit

---
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -0,0 +1,13 @@
+package gm.zona_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitApplication {
+
+	public static void main(String[] args) {
+		// System.out.println("Hola Spring boot");
+		SpringApplication.run(ZonaFitApplication.class, args);
+	}
+}
